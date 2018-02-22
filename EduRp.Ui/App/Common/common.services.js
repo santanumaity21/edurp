5f59b0b4b2578526ca0c5f5ea9f08d0e2da3e5b3
Santanu Maity <santanumaity21@gmail.com>
2018-02-22 22:30:36 +0530

    latest azure ui

---
+++ EduRp.Ui/App/Common/common.services.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services",
+        ["ngResource"])
+        .constant("appSettings",
+        {
+            serverPath: "http://edurpwebapi20180213021849.azurewebsites.net"
+        });
+}());
+
+//http://edurpwebapi20180213021849.azurewebsites.net
+//http://localhost:50381 
---
+++ EduRp.Ui/App/Controllers/StudentAdmissionForm/studentadmissionformController.js
@@ -0,0 +1,204 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('EduRpApp')
+        .controller('studentAdmissionFormController', studentAdmissionFormController);
+
+    studentAdmissionFormController.$inject = ['$scope', '$q', '$log', 'errorHandler', 'studentAdmissionFormService', 'commonService', '$translate', 'programStudyService','managecourseService'];
+
+    function studentAdmissionFormController($scope, $q, $log, errorHandler, studentAdmissionFormService, commonService, $translate, programStudyService, managecourseService) {
+
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'studentAdmissionFormController';
+        $scope.oneAtATime = true;
+        $scope.admissionFormData = [];
+        $scope.courseData = [];
+        $scope.subjectData = [];
+
+        $scope.selectedProgramStudy = null;
+        $scope.selectedCourse = null;
+        $scope.selectedSubject = null;
+        $scope.programStudyData = [];
+        $scope.courseListDetails = undefined;
+
+        $scope.modAdmissionObj = {};
+
+        $scope.$watch('admissionFormData[0].open', function (isOpen) {
+            if (isOpen) {
+
+                console.log('First group was opened');
+            }
+        });
+
+        $scope.$watch('admissionFormData[1].open', function (isOpen) {
+            if (isOpen) {
+                
+                console.log('Second group was opened');
+            }
+        });
+        // accordion 3 open 
+        $scope.$watch('admissionFormData[2].open', function (isOpen) {
+         
+            if (isOpen) {
+
+                $q.all([
+                    programStudyService.getProgramStudyList()
+                ]).then(function (data) {
+                    $scope.mainContent = true;
+                    if (data != null) {
+                        $scope.programStudyData = data[0].results;
+                    }
+                }, function (reason) {
+                    console.log("reason" + reason);
+                    errorHandler.logServiceError('programStudyController', reason);
+                }, function (update) {
+                    console.log("update" + update);
+                    errorHandler.logServiceNotify('programStudyController', update);
+                });
+
+                console.log('Third group was opened');
+            }
+        });
+
+        $scope.$watch('admissionFormData[3].open', function (isOpen) {
+            if (isOpen) {
+                $q.all([
+                    studentAdmissionFormService.getApplicationFormFeeDetail()
+                ]).then(function (data) {
+                    $scope.mainContent = true;
+                    if (data != null) {
+                        $scope.feesData = data[0].results;
+                    }
+                }, function (reason) {
+                    console.log("reason" + reason);
+                    errorHandler.logServiceError('programStudyController', reason);
+                }, function (update) {
+                    console.log("update" + update);
+                    errorHandler.logServiceNotify('programStudyController', update);
+                });
+
+            }
+        });
+
+        $scope.$watch('admissionFormData[4].open', function (isOpen) {
+            if (isOpen) {
+
+                $q.all([
+                    studentAdmissionFormService.getApplicationFormHeader()
+                ]).then(function (data) {
+                    $scope.mainContent = true;
+                    if (data != null) {
+                        $scope.paymentsData = data[0].results;
+                    }
+                }, function (reason) {
+                    console.log("reason" + reason);
+                    errorHandler.logServiceError('programStudyController', reason);
+                }, function (update) {
+                    console.log("update" + update);
+                    errorHandler.logServiceNotify('programStudyController', update);
+                });
+            }
+        });
+
+        $scope.$on('topic', function (event, arg) {
+            $scope.receiver = 'got your ' + arg;
+            console.log($scope.receiver);
+        });
+
+
+        $scope.addAdmissionFormDetails = function (form) {
+                $q.when([studentAdmissionFormService.addStudentAdmissionForm($scope.modAdmissionObj)]).then(function (data) {
+                    
+                }, function (error) {
+
+                });
+
+        };
+
+        $scope.getAdmissionNumber = function (form) {
+            $q.when([studentAdmissionFormService.getAdmissionNumber()]).then(function (data) {
+               
+            }, function (error) {
+
+            });
+
+        };
+
+        //function activate() { }
+        (function startup() {
+
+            $q.all([
+                studentAdmissionFormService.getDynamicFormData()
+            ]).then(function (data) {
+                if (data !== null) {
+                    $scope.admissionFormData =[];
+                    angular.forEach(data[0].results, function (std, stk) {
+                        var found = 0;
+                        angular.forEach($scope.admissionFormData, function (admv, admk) {
+                            if (admv.AppFormGroupId === std.AppFormGroupId) {
+                                found = 1;
+                                $scope.admissionFormData[admk]['fields'].push({ AppFormFieldId: std.AppFormFieldId, FieldName: std.FieldName, Value: std.Value});
+                            };
+                        });
+                        if (found === 0) {
+                            $scope.admissionFormData.push({ AppFormGroupId: std.AppFormGroupId, AppFormGroupLabel: std.AppFormGroupLabel, "fields": [{ AppFormFieldId: std.AppFormFieldId, FieldName: std.FieldName, Value: std.Value }]})
+                        }
+
+                    });
+                    console.log($scope.admissionFormData);
+                }
+            }, function (reason) {
+                errorHandler.logServiceError('studentAdmissionFormController', reason);
+            }, function (update) {
+                errorHandler.logServiceNotify('studentAdmissionFormController', update);
+            });
+
+        })();
+
+//accordion 3 functionality
+
+        $scope.fetchRelatedDataOfPS = function (psData) {
+            var selPS = angular.copy(psData);
+            if (selPS) {
+                $q.all([
+                    programStudyService.getLinkedCoursesOfProgramStudy(selPS)
+                ]).then(function (data) {
+                   // $scope.mainContentSubPart = true;
+                    if (data != null) { 
+                        $scope.courseData = data[0].results;
+                      
+                    }
+                }, function (reason) {
+                    errorHandler.logServiceError('programStudyController', reason);
+                });
+            } else {
+                alert("Please select a course");
+            }
+        };
+
+        $scope.fetchRelatedDataOfCourse = function (psData) {
+            var selPS = angular.copy(psData);
+            if (selPS) {
+                $q.all([
+                    managecourseService.getCourseSubjectList(selPS)
+                ]).then(function (data) {
+                    // $scope.mainContentSubPart = true;
+                    if (data != null) {
+                        $scope.subjectData = data[0].results;
+                    }
+                }, function (reason) {
+                    errorHandler.logServiceError('manageCourseController', reason);
+                });
+            } else {
+                alert("Please select a course");
+            }
+        };
+
+        $scope.resetForm = function () {
+            $scope.student = angular.copy($scope.OrginalAdmissionObj);
+        };
+    };
+
+})();
